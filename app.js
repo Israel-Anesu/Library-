624712e329d71620b92454f62eb89322710fe432
israel-velocity <israel@velocityinc.tech>
2023-01-24 10:09:52 +0200

    Updating dummy data books

--- app.js
+++ app.js
@@ -144,10 +144,10 @@ function changeBookStatus(event) {
 }
 
 function addDemoData() {
-  addBookToLibrary('The Last Wish', 'Andrzej Sapkowski', '288', true);
-  addBookToLibrary('Sword of Destiny', 'Andrzej Sapkowski', '384', true);
-  addBookToLibrary('Blood of Elves', 'Andrzej Sapkowski', '320', true);
-  addBookToLibrary('Time of Contempt', 'Andrzej Sapkowski', '331', true);
+  addBookToLibrary('Nervous Conditions', 'Tsitsi Dangarebga', '288', true);
+  addBookToLibrary('The Grass Is Singing', 'Doris Lessing', '384', true);
+  addBookToLibrary('The Stone Virgins', 'Yvonne Vera', '320', true);
+  addBookToLibrary('Butterfly Burning', 'Yvonne Vera', '331', true);
   addBookToLibrary('Baptism of Fire', 'Andrzej Sapkowski', '343', true);
   addBookToLibrary('The Tower of the Swallow', 'Andrzej Sapkowski', '436', false);
   addBookToLibrary('The Lady of the Lake', 'Andrzej Sapkowski', '531', false);
